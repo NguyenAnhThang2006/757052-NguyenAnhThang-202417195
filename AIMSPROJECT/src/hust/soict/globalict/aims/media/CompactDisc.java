44d7ea4acc3bf268ab3a23565ab42c7324af9c06
NguyenAnhThang2006 <thang.nguyenanh1974@gmail.com>
2025-12-11 01:42:22 +0700

    Update lab05

---
+++ AIMSPROJECT/src/hust/soict/globalict/aims/media/CompactDisc.java
@@ -0,0 +1,56 @@
+package hust.soict.globalict.aims.media;
+
+import hust.soict.globalict.aims.playable.Playable;
+import hust.soict.globalict.aims.exception.PlayerException; // Cần import PlayerException
+import java.util.ArrayList;
+
+public class CompactDisc extends Disc implements Playable {
+
+    private String artist;
+    private ArrayList<Track> tracks = new ArrayList<>();
+
+    public CompactDisc(String title, String category, float cost,
+                       String director, int length, String artist) throws IllegalArgumentException {
+        super(title, category, cost, director, length);
+        this.artist = artist;
+    }
+
+    public void addTrack(Track track) {
+        if (!tracks.contains(track)) tracks.add(track);
+    }
+
+    public void removeTrack(Track track) {
+        tracks.remove(track);
+    }
+
+    @Override
+    public int getLength() {
+        int total = 0;
+        if (tracks != null) {
+            for (Track t : tracks) {
+                total += t.getLength();
+            }
+        }
+        return total;
+    }
+
+    @Override
+    public void play() throws PlayerException {
+        if (this.getLength() <= 0) {
+            System.err.println("ERROR: CD total length is non-positive!");
+            throw new PlayerException("ERROR: CD length is non-positive!");
+        }
+
+        System.out.println("Playing CD: " + this.getTitle());
+        System.out.println("Artist: " + artist);
+
+        for (Track track : tracks) {
+            try {
+                track.play();
+            } catch (PlayerException e) {
+                System.err.println("CD Play Error: Track '" + track.getTitle() + "' failed to play.");
+                throw e;
+            }
+        }
+    }
+}
---
+++ AIMSPROJECT/src/hust/soict/globalict/aims/media/Track.java
@@ -0,0 +1,46 @@
+package hust.soict.globalict.aims.media;
+
+import hust.soict.globalict.aims.playable.Playable;
+import hust.soict.globalict.aims.exception.PlayerException; // Cần import PlayerException
+
+public class Track implements Playable {
+    private String title;
+    private int length;
+
+    public Track(String title, int length) throws IllegalArgumentException {
+        if (length < 0) {
+            throw new IllegalArgumentException("ERROR: Track length cannot be negative.");
+        }
+        this.title = title;
+        this.length = length;
+    }
+
+    public String getTitle() { return title; }
+    public int getLength() { return length; }
+
+    @Override
+    public void play() throws PlayerException { // Cập nhật chữ ký phương thức
+        if (this.length > 0) {
+            System.out.println("Playing Track: " + title);
+            System.out.println("Track length: " + length);
+        } else {
+            // Ném PlayerException nếu length không dương
+            System.err.println("ERROR: Track length is non-positive!");
+            throw new PlayerException("ERROR: Track length is non-positive!");
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !(obj instanceof Track track)) {
+            return false;
+        }
+
+
+        return this.length == track.length && this.title.equalsIgnoreCase(track.title);
+    }
+}
